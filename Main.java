dc3d16b24c16896fbcd3616e2a37fdbc5831bfd8
dlwlrja <kazakovavladillava@gmail.com>
2024-03-25 10:34:39 +0100

    /

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] weight = new int[10];
+        for (int i = 0; i < weight.length; i++) {
+            weight[i] = new Random().nextInt(40,100);
+            System.out.println(weight[i]);
+
+        }
+        int summ = 0;
+        for (int i = 0; i < weight.length; i++){
+            summ = summ + weight[i] ;
+            if (weight[i]>=60 && weight[i]<=80){
+                System.out.println("Вес в диапазоне от 60 до 80: " + weight[i]);
+            }
+        } System.out.println("Средний вес: "+ summ/ weight.length);
+
+
+    }
+}
